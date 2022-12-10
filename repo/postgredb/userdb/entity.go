622cf95c4aa52086a19bbea869e82b922194acab
sarsembin <daur2000@gmail.com>
2022-12-10 09:48:40 +0600

    fix

--- repo/postgredb/userdb/entity.go
+++ repo/postgredb/userdb/entity.go
@@ -8,10 +8,10 @@ type Entity struct {
 	ID          int       `pg:"id,pk"`
 	Password    string    `pg:"password,notnull"`
 	LastLogin   time.Time `pg:"last_login"`
-	IsSuperuser bool      `pg:"is_superuser"`
+	IsSuperuser bool      `pg:"is_superuser,use_zero"`
 	FirstName   string    `pg:"first_name,unique"`
 	LastName    string    `pg:"last_name"`
-	IsStaff     bool      `pg:"is_staff"`
+	IsStaff     bool      `pg:"is_staff,use_zero"`
 	IsActive    bool      `pg:"is_active"`
 	DateJoined  time.Time `pg:"date_joined"`
 	Username    string    `pg:"username,unique,notnull"`
